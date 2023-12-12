9452aa7db9053c1a2778aac032fe0e39bf4da286
Pedro Brandão <phbrandao@outlook.com>
2023-12-12 19:14:37 -0300

    Implementando CRUD de Admin

---
+++ app/Http/Controllers/Api/AdminController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $admins = Admin::all();
+
+        return response()->json([
+            'admins: ' => $admins,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'name' => ['required'],
+            'phone' => ['required'],
+            'socialNumber' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        $input['password']= bcrypt($input['password']);
+        $admin = Admin::create($input);
+
+        if($admin->save()) {
+            return response()->json([
+                'message: ' => 'Admin created!',
+            ], 200);
+        } else {
+            return response([
+                'message: ' => 'Admin not created.',
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $admin = Admin::find($id);
+
+        if(is_null($admin)) {
+            return response()->json([
+                "message" => "Admin not found!"
+            ], 500);
+        }
+
+        return response()->json([
+            'admin: ' => $admin
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $admin = Admin::find($id);
+
+        if($admin){
+           $input = $request->validate([
+              'name' => ['required'],
+              'phone' => ['required'],
+              'socialNumber' => ['required'],
+              'password' => ['required'],
+            ]);
+
+            $admin->name = $input['name'];
+            $admin->phone = $input['phone'];
+            $admin->socialNumber = $input['socialNumber'];
+            $admin->password = $input['password'];
+
+            if($admin->save()){
+                return response()->json([
+                    'message: ' => 'Admin updated!',
+                    'admin: ' => $admin
+                ], 200);
+            }else {
+                return response([
+                    'message: ' => 'Admin not updated!',
+                ], 500);
+            }
+        } else {
+            return response([
+                'message: ' => 'Admin not found!',
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $admin = Admin::find($id);
+
+        if($admin){
+            $admin->delete();
+
+            return response()->json([
+                'message: ' => 'Admin deleted!',
+            ], 200);
+        } else {
+            return response([
+                'message: ' => 'Admin not found!',
+            ], 500);
+        }
+    }
+}
---
+++ app/Models/Admin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'phone',
+        'socialNumber',
+        'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password'
+    ];
+}
